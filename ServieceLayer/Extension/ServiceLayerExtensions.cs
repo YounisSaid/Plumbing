f375211bb86437422b950c2d2f780c2b6093bd90
Younis Said <Younissaid900@gmail.com>
2025-11-19 01:33:22 +0200

    Added ServiceLayerExtension Method

---
+++ ServieceLayer/Extension/ServiceLayerExtensions.cs
@@ -0,0 +1,15 @@
+﻿
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace ServieceLayer.Extension
+{
+    public static class ServiceLayerExtensions
+    {
+        public static IServiceCollection LoadServiceLayerExtensions(this IServiceCollection services)
+        {
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+            return services;
+        }
+    }
+}
