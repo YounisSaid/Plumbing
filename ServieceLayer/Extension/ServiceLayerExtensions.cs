0520af7d449defe553b470c7aaa10c91af9769d8
Younis Said <Younissaid900@gmail.com>
2025-11-20 22:42:33 +0200

    Added Generic method to add Services Automaticaly

--- ServieceLayer/Extension/ServiceLayerExtensions.cs
+++ ServieceLayer/Extension/ServiceLayerExtensions.cs
@@ -9,8 +9,18 @@ namespace ServieceLayer.Extension
         public static IServiceCollection LoadServiceLayerExtensions(this IServiceCollection services)
         {
             services.AddAutoMapper(Assembly.GetExecutingAssembly());
+            var types = Assembly.GetExecutingAssembly().GetTypes()
+                .Where(t => t.IsClass && !t.IsAbstract && t.Name.EndsWith("Service"));
+            foreach (var type in types)
+            {
+                var interfaceType = type.GetInterfaces().FirstOrDefault(x => x.Name == $"I{type.Name}");
+                if (interfaceType != null)
+                {
+                    services.AddScoped(interfaceType, type);
+                }
 
-                return services;
+            }
+            return services;
         }
     }
 }
