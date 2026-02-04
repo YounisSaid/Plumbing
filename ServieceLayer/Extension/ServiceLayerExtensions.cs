8e976f6fce42a1d014e9f435e347ed49ea24de45
Younis Said <Younissaid900@gmail.com>
2026-02-04 12:13:53 +0200

    Added Scrutor to register Services

--- ServieceLayer/Extension/ServiceLayerExtensions.cs
+++ ServieceLayer/Extension/ServiceLayerExtensions.cs
@@ -6,6 +6,10 @@ using Microsoft.Extensions.DependencyInjection;
 using ServiceLayer.Extension.Identity;
 using ServiceLayer.FluentValidation.WebApplication.HomePageValidation;
 using ServiceLayer.Helpers.Generic;
+using ServiceLayer.Serviecs.Identity.Abstract;
+using ServiceLayer.Serviecs.Identity.Concrete;
+using ServiceLayer.Serviecs.WebApplication.Abstract;
+using ServiceLayer.Serviecs.WebApplication.Concrete;
 using System.Reflection;
 
 namespace ServiceLayer.Extension
@@ -17,16 +21,18 @@ namespace ServiceLayer.Extension
             services.LoadIdentityExtention(configuration);
             services.AddAutoMapper(Assembly.GetExecutingAssembly());
 
-            var types = Assembly.GetExecutingAssembly().GetTypes()
-                .Where(t => t.IsClass && !t.IsAbstract && t.Name.EndsWith("Service"));
-            foreach (var type in types)
-            {
-                var interfaceType = type.GetInterfaces().FirstOrDefault(x => x.Name == $"I{type.Name}");
-                if (interfaceType != null)
-                {
-                    services.AddScoped(interfaceType, type);
-                }
-            }
+           
+
+            // Register Services With Scrutor
+            services.Scan(scan => scan
+                                .FromAssemblyOf<AboutService>()
+                                .AddClasses()
+                                .AsMatchingInterface()
+                                .WithScopedLifetime());
+
+
+        
+
 
             services.AddFluentValidationAutoValidation(opt => 
                                                    opt.DisableDataAnnotationsValidation = true);
