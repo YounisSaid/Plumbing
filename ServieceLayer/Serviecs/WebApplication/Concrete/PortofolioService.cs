568195120e6c13cbd29d24f90912c033be6b9fa9
Younis Said <Younissaid900@gmail.com>
2026-02-08 15:37:03 +0200

    Added Portiflio ImageHelper

--- ServieceLayer/Serviecs/WebApplication/Concrete/PortofolioService.cs
+++ ServieceLayer/Serviecs/WebApplication/Concrete/PortofolioService.cs
@@ -1,10 +1,12 @@
 ﻿using AutoMapper;
 using AutoMapper.QueryableExtensions;
+using EntityLayer.Enumerates;
 using EntityLayer.WebApplication.Entites;
 using EntityLayer.WebApplication.ViewModels.Portfolio;
 using Microsoft.EntityFrameworkCore;
 using RepositoryLayer.Repositories.Abstract;
 using RepositoryLayer.UnitOfWorks.Abstract;
+using ServiceLayer.Helpers.Generic;
 using ServiceLayer.Serviecs.WebApplication.Abstract;
 
 namespace ServiceLayer.Serviecs.WebApplication.Concrete
@@ -14,12 +16,15 @@ namespace ServiceLayer.Serviecs.WebApplication.Concrete
         private readonly IUnitOfWork _unitOfWork;
         private readonly IMapper _mapper;
         private readonly IGenericRepository<Portfolio> _portfolioRepository;
+        public readonly IImageHelper _imageHelper;
 
-        public PortfolioService(IUnitOfWork unitOfWork, IMapper mapper)
+
+        public PortfolioService(IUnitOfWork unitOfWork, IMapper mapper, IImageHelper imageHelper)
         {
             _unitOfWork = unitOfWork;
             _mapper = mapper;
             _portfolioRepository = _unitOfWork.GetRepository<Portfolio>();
+            _imageHelper = imageHelper;
         }
 
         public async Task<List<PortfolioListMV>> GetAllListAsync()
@@ -38,6 +43,13 @@ namespace ServiceLayer.Serviecs.WebApplication.Concrete
 
         public async Task AddPortfolioAsync(PortfolioAddMV addMV)
         {
+            var image = await _imageHelper.UploadImageAsync(null, addMV.Photo, imageType.portifolio);
+            if (image.Error != null)
+            {
+                return;
+            }
+            addMV.FileName = image.FileName!;
+            addMV.FileType = image.FileType!;
             var portfolio = _mapper.Map<Portfolio>(addMV);
             await _portfolioRepository.AddAsync(portfolio);
             await _unitOfWork.CommitAsync();
@@ -45,9 +57,25 @@ namespace ServiceLayer.Serviecs.WebApplication.Concrete
 
         public async Task UpdatePortfolioAsync(PortfolioUpdateMV updateMV)
         {
+            var oldpPortfolio = await _portfolioRepository.Where(x => x.Id == updateMV.Id).AsNoTracking().FirstAsync();
+            if (updateMV.Photo != null)
+            {
+                var image = await _imageHelper.UploadImageAsync(null, updateMV.Photo, imageType.about);
+                if (image.Error != null)
+                {
+                    return;
+                }
+                updateMV.FileName = image.FileName!;
+                updateMV.FileType = image.FileType!;
+            }
             var portfolio = _mapper.Map<Portfolio>(updateMV);
             _portfolioRepository.Update(portfolio);
             await _unitOfWork.CommitAsync();
+
+            if (updateMV.Photo != null)
+            {
+                _imageHelper.DeleteImage(oldpPortfolio!.FileName);
+            }
         }
 
         public async Task DeletePortfolioAsync(int id)
@@ -55,6 +83,8 @@ namespace ServiceLayer.Serviecs.WebApplication.Concrete
             var portfolio = await _portfolioRepository.GetByIdAsync(id);
             _portfolioRepository.Delete(portfolio!);
             await _unitOfWork.CommitAsync();
+            _imageHelper.DeleteImage(portfolio!.FileName);
+
         }
     }
 }
