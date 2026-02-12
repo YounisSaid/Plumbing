dc6070ded3216721abc83831ae51959ac0a62fb0
Younis Said <Younissaid900@gmail.com>
2026-02-12 12:58:23 +0200

    Added UserLayout

--- Plumbing.MVC/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Plumbing.MVC.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
